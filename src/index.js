5eef37790f6e5c7ee94d791919e428d1fff1ae52
Amine derbal <aminedoran31@gmail.com>
2023-03-13 17:30:04 +0100

    add call to api and display items when page load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles/style.css';
+import { getApiItems } from './modules/baseApi.js';
+import displayItems from './modules/display.js';
+
+window.onload = async () => {
+  const items = await getApiItems();
+  displayItems(await items);
+};
