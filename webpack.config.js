38c90f851804138427e686f70ba2a62c7702c257
Amine derbal <aminedoran31@gmail.com>
2023-03-13 15:01:22 +0100

    add config for pictures

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   devtool: 'inline-source-map',
