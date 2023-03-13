175e4745ea4d2a77c02094195c96a47122c0e079
Amine derbal <aminedoran31@gmail.com>
2023-03-13 17:29:31 +0100

    add function to display items

---
+++ src/modules/display.js
@@ -0,0 +1,14 @@
+import likeSrc from '../image/like-icon.png';
+
+const displayItems = (itemslist) => {
+  const itemsContainer = document.getElementById('items');
+  itemslist.forEach((item, index) => {
+    const itemElement = document.createElement('div');
+    itemElement.className = 'item';
+    itemElement.setAttribute('data-index', index);
+    itemElement.innerHTML = `<img class="item-img" src=${item.url} alt=${item.breeds[0].name} /> <div><p>${item.breeds[0].name}</p><div><img class="like-icon" src=${likeSrc} alt="like" /> <p> <span>0</span> likes </p></div> </div><button>comment</button>`;
+    itemsContainer.appendChild(itemElement);
+  });
+};
+
+export default displayItems;
