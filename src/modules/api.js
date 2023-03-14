3467149d10e8c470aefdf9600b9168e1c4322f98
Amine derbal <aminedoran31@gmail.com>
2023-03-14 11:19:46 +0100

    add send a like and get all likes functions

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,7 +1,9 @@
-const url = 'https://api.thedogapi.com/v1/images/search?format=json&order=ASC&limit=10';
+const baseUrl = 'https://api.thedogapi.com/v1/images/search?format=json&order=ASC&limit=10';
+const involveUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const involveId = 'SGBNwoFbgJ2ty7olqkAP';
 
 export const getApiItems = async () => {
-  const dataStream = await fetch(url, {
+  const dataStream = await fetch(baseUrl, {
     headers: {
       'x-api-key': 'live_CcSfcJy9YCjnG2GgcCo196zIno3Kj7xVxdfu7lIGZGVZrdTQWqlE9UcCqX1JW7XY',
     },
@@ -9,4 +11,24 @@ export const getApiItems = async () => {
   const dataResponse = await dataStream.json();
   return dataResponse;
 };
-export default getApiItems;
+
+export const getAllLikesData = async () => {
+  const dataStream = await fetch(`${involveUrl}${involveId}/likes/`);
+  const dataResponse = await dataStream.json();
+  return dataResponse;
+};
+
+export const sendALike = async (index) => {
+  const dataStream = await fetch(`${involveUrl}${involveId}/likes/`, {
+    method: 'POST',
+    headers: {
+      'content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      item_id: index,
+    }),
+  });
+  return dataStream;
+};
+
+export default { getApiItems, sendALike, getAllLikesData };
