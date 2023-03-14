3bea18f8c937b23bca76193ba2f4a1afae49f67b
Amine derbal <aminedoran31@gmail.com>
2023-03-14 08:42:59 +0100

    add some classes names to the item display

--- src/modules/display.js
+++ src/modules/display.js
@@ -6,7 +6,7 @@ const displayItems = (itemslist) => {
     const itemElement = document.createElement('div');
     itemElement.className = 'item';
     itemElement.setAttribute('data-index', index);
-    itemElement.innerHTML = `<img class="item-img" src=${item.url} alt=${item.breeds[0].name} /> <div><p>${item.breeds[0].name}</p><div><img class="like-icon" src=${likeSrc} alt="like" /> <p> <span>0</span> likes </p></div> </div><button>comment</button>`;
+    itemElement.innerHTML = `<img class="item-img" src=${item.url} alt=${item.breeds[0].name} /> <div class="item-description"><p>${item.breeds[0].name}</p><div><img class="like-icon" src=${likeSrc} alt="like" /> <p> <span>0</span> likes </p></div> </div><button class="comment-button">comment</button>`;
     itemsContainer.appendChild(itemElement);
   });
 };
