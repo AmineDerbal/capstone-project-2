d3324ee44e2f85e605056b148868531306290e94
QueenterJuma <jumahqueenter@gmail.com>
2023-03-15 12:19:03 +0300

    Fixed eslint linter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "./styles/style.css";
-import { getApiItems } from "./modules/api.js";
-import displayItems from "./modules/display.js";
+import './styles/style.css';
+import { getApiItems } from './modules/api.js';
+import displayItems from './modules/display.js';
 
 window.onload = async () => {
   const items = await getApiItems();
