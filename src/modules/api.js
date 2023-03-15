aa9538c734d3a17e28902c13012ddd6dd38e7794
QueenterJuma <jumahqueenter@gmail.com>
2023-03-15 12:07:35 +0300

    Add update on api.js

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,14 +1,11 @@
-const baseUrl =
-  "https://api.thedogapi.com/v1/images/search?format=json&order=ASC&limit=10";
-const involveUrl =
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/";
-const involveId = "SGBNwoFbgJ2ty7olqkAP";
+const baseUrl = 'https://api.thedogapi.com/v1/images/search?format=json&order=ASC&limit=10';
+const involveUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const involveId = 'SGBNwoFbgJ2ty7olqkAP';
 
 export const getApiItems = async () => {
   const dataStream = await fetch(baseUrl, {
     headers: {
-      "x-api-key":
-        "live_CcSfcJy9YCjnG2GgcCo196zIno3Kj7xVxdfu7lIGZGVZrdTQWqlE9UcCqX1JW7XY",
+      'x-api-key': 'live_CcSfcJy9YCjnG2GgcCo196zIno3Kj7xVxdfu7lIGZGVZrdTQWqlE9UcCqX1JW7XY',
     },
   });
   const dataResponse = await dataStream.json();
@@ -16,9 +13,10 @@ export const getApiItems = async () => {
 };
 
 export const getApiComments = async (index) => {
-  const dataStream = await fetch(
-    `${involveUrl}${involveId}/comments?item_id=${index}`
-  );
+  const dataStream = await fetch(`${involveUrl}${involveId}/comments?item_id=${index}`);
+  if (dataStream.status !== 200) {
+    return [];
+  }
   const dataResponse = await dataStream.json();
   return dataResponse;
 };
@@ -31,9 +29,9 @@ export const getAllLikesData = async () => {
 
 export const sendALike = async (index) => {
   const dataStream = await fetch(`${involveUrl}${involveId}/likes/`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "content-Type": "application/json; charset=UTF-8",
+      'content-Type': 'application/json; charset=UTF-8',
     },
     body: JSON.stringify({
       item_id: index,
@@ -42,4 +40,6 @@ export const sendALike = async (index) => {
   return dataStream;
 };
 
-export default { getApiItems, sendALike, getAllLikesData, getApiComments };
+export default {
+  getApiItems, sendALike, getAllLikesData, getApiComments,
+};
