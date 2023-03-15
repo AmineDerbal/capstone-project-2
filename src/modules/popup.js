947657c05d45b1ec93da389dbfcc82c5160c03d7
QueenterJuma <jumahqueenter@gmail.com>
2023-03-15 12:08:51 +0300

    Add update in popup.js file

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,8 +1,14 @@
-import getApiComments from "./api.js";
+import { getApiComments } from './api.js';
 
-const displayPopup = (item, index) => {
-  const modal = document.createElement("div");
-  modal.id = "modal";
+const appendComment = (commentItem) => {
+  const list = document.createElement('li');
+  list.innerHTML = `<span class ="date">${commentItem.creation_date} </span> <span class="name">${commentItem.username} </span> <span class="comment">${commentItem.comment}</span>`;
+  return list;
+};
+
+const displayPopup = async (item, index) => {
+  const modal = document.createElement('div');
+  modal.id = 'modal';
   modal.innerHTML = `<div class="modal-content">
         <span id='close-btn' class="close close-btn">&times;</span>
         <div ><img class="pop-image" src=${item.url} alt=${item.breeds[0].name}></div>
@@ -21,8 +27,8 @@ const displayPopup = (item, index) => {
             <ul id="listcoment" class="listcoment"></ul>
             </div>
           <div class="d-comment">
-            <h4>Add a d-Comment</h4>
-            <form action="https://formspree.io/f/mqkjggay" method="post" id="form">
+            <h4>Add a d-comment</h4>
+            <form id="form">
               <label for="name"></label>
               <input type="text" id="name" name="name" maxlength="30" placeholder="Name" required/>
               <textarea type="text" id="text" maxlength="500" rows="8" cols="50" placeholder="Write your comment here" required></textarea>
@@ -32,12 +38,16 @@ const displayPopup = (item, index) => {
         </div>
       </div>`;
   document.body.appendChild(modal);
-  const commentsList = document.getElementById("listcoment");
-  const comments = getApiComments(index);
-  console.log(comments);
+  const commentsList = document.getElementById('listcoment');
+  const comments = await getApiComments(index);
+  if (comments.length !== 0) {
+    comments.forEach((comment) => {
+      commentsList.appendChild(appendComment(comment));
+    });
+  }
 
-  const closeBtn = document.querySelector(".close-btn");
-  closeBtn.addEventListener("click", () => {
+  const closeBtn = document.querySelector('.close-btn');
+  closeBtn.addEventListener('click', () => {
     modal.parentElement.removeChild(modal);
   });
 };
