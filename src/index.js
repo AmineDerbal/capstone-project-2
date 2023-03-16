f11181e51fc819ae3abaa23b3ed9a3d99c1240e1
Amine derbal <aminedoran31@gmail.com>
2023-03-16 10:07:47 +0100

    load the logo when page is loaded

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import './styles/style.css';
 import 'animate.css';
+import logoSrc from './image/logo.jpg';
 
 import { getApiItems } from './modules/api.js';
 import displayItems from './modules/display.js';
 
+const loadLogo = () => {
+  const logoImage = document.querySelector('#logo img');
+  logoImage.src = logoSrc;
+};
+
 const getItemsCount = () => document.querySelectorAll('.item').length;
 window.onload = async () => {
+  loadLogo();
   const items = await getApiItems();
   displayItems(items);
   const itemsCounter = document.getElementById('item-counters');
